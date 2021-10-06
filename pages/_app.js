2748376708463df65c1c64d682c169ac9264692a
gianw <gian.cb@hotmail.com>
2021-10-05 23:13:05 -0300

    Remove unused files

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
-// import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import {theme} from '../src/theme';
 import CssBaseline from '@mui/material/CssBaseline';
-// import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
