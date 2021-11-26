f3f9964a96298185669dfae724eeb90af7a4508f
gianw <gian.cb@hotmail.com>
2021-11-25 22:24:13 -0300

    Add prism

--- components/post-body.js
+++ components/post-body.js
@@ -1,17 +1,30 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
-
+import Head from 'next/head'
 import markdownStyles from './markdown-styles.module.css'
 
 export const PostBody = ({ content }) => {
+  const theme = 'tomorrow'
   return (
-    // <div className='max-w-2xl mx-auto'>
-    <div>
-      <div
-        className={markdownStyles['markdown']}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </div>
+    <>
+      <Head>
+        <link
+          rel='preload'
+          href='https://unpkg.com/prismjs@0.0.1/themes/prism-tomorrow.css'
+          as='script'
+        />
+        <link
+          href={`https://unpkg.com/prismjs@0.0.1/themes/prism-${theme}.css`}
+          rel='stylesheet'
+        />
+      </Head>
+      <div className='max-w-2xl mx-auto'>
+        <div
+          className={markdownStyles['markdown']}
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </div>
+    </>
   )
 }
 
