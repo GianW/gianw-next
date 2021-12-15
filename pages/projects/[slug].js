4672787558c78f6776fa1778fb13eb106ad634c7
gianw <gian.cb@hotmail.com>
2021-12-14 23:32:58 -0300

    Ajustes post contentfull

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,87 +1,59 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
-// import { useRouter } from 'next/router'
-// import Head from 'next/head'
-// import ErrorPage from 'next/error'
-
-// import Container from '../../components/container'
-// import PostBody from '../../components/post-body'
-// import Header from '../../components/header'
-// import PostHeader from '../../components/post-header'
-// import SectionSeparator from '../../components/section-separator'
-// import Layout from '../../components/layout'
-// import PostTitle from '../../components/post-title'
-
-// import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import { Container } from '@mui/material'
+import { AppHeader } from '/components/AppHeader'
+import Head from 'next/head'
+import { Seo } from '/components/Seo'
 
 import { getAllPostsWithSlug, getPost } from '/lib/contentFullApi'
-
-// import ReactMarkdown from 'react-markdown/react-markdown.min'
-// import remarkGfm from 'remark-gfm'
 
 import { PostBody } from '/components/post-body'
 import markdownToHtml from '/lib/markdownToHtml'
 
-export default function Post({ post }) {
-  // const router = useRouter()
+export default function Post({ content, post }) {
+  const router = useRouter()
 
-  // if (!router.isFallback && !post) {
-  //   return <ErrorPage statusCode={404} />
-  // }
+  if (!router.isFallback && !post) {
+    return <ErrorPage statusCode={404} />
+  }
 
   return (
-    // <Layout preview={preview}>
-    //   <Container>
-    //     <Header />
-    //     {router.isFallback ? (
-    //       <PostTitle>Loading…</PostTitle>
-    //     ) : (
-    //       <>
-    //         <article>
-    //           <Head>
-    //             <title>{post.title} | Next.js Blog Example with</title>
-    //             <meta property='og:image' content={post.coverImage.url} />
-    //           </Head>
-    //           <PostHeader
-    //             title={post.title}
-    //             coverImage={post.coverImage}
-    //             date={post.date}
-    //             author={post.author}
-    //           />
-    //           <PostBody content={post.content} />
-    //         </article>
-    //         <SectionSeparator />
-    //       </>
-    //     )}
-    //   </Container>
-    // </Layout>
-
-    // <pre>{JSON.stringify(post, null, 2)}</pre>
-    // <pre>
-    // {JSON.stringify(post?.post.json.content[0].content[0].value, null, 2)}
-    // <pre>{JSON.stringify(post, null, 2)}</pre>
-    <PostBody content={post} />
-    // <ReactMarkdown remarkPlugins={[remarkGfm]}>{post}</ReactMarkdown>
+    <>
+      <AppHeader />
+      <Container>
+        {router.isFallback ? (
+          <>
+            <p>Loading…</p>
+          </>
+        ) : (
+          <>
+            <Head>
+              <title>{content.titulo}</title>
+              <Seo keywords={content.seo} />
+            </Head>
+            <PostBody content={post} />
+          </>
+        )}
+      </Container>
+    </>
   )
 }
 Post.propTypes = {
+  content: PropTypes.object,
   post: PropTypes.object,
-  // preview: PropTypes.bool,
 }
 
 export async function getStaticProps({ params, preview = false }) {
   const data = await getPost(params.slug, preview)
-  const dados = data?.post.postTwo
-  // const dados = data?.post.post.json.content[0].content[0].value
-  const content = await markdownToHtml(dados || '')
-  // const content = dados
+  const buildedContent = await markdownToHtml(data?.post.post || '')
 
   return {
     props: {
       preview,
-      post: content,
-      // post: data?.post ?? null,
-      // morePosts: data?.morePosts ?? null,
+      post: buildedContent,
+      content: data?.post,
     },
   }
 }
