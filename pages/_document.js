57abe4acc316ca9208c65bec5fd5ae3b184f3079
GianW <gian.cb@hotmail.com>
2022-03-13 22:06:53 -0300

    Add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,7 @@ import React from 'react'
 import NextDocument from 'next/document'
 import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
 import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@mui/styles'
+import { GA_TRACKING_ID } from 'lib/gtag'
 
 export default class Document extends NextDocument {
   static async getInitialProps(ctx) {
@@ -14,7 +15,27 @@ export default class Document extends NextDocument {
         originalRenderPage({
           enhanceApp: App => props =>
             styledComponentSheet.collectStyles(
-              materialUiSheets.collect(<App {...props} />)
+              materialUiSheets.collect(
+                <>
+                  <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                  />
+                  <script
+                    dangerouslySetInnerHTML={{
+                      __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${GA_TRACKING_ID}', {
+                          page_path: window.location.pathname,
+                        });
+                      `,
+                    }}
+                  />
+                  <App {...props} />
+                </>
+              )
             ),
         })
 
