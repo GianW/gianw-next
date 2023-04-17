8883c80c1021abc0b062e7840011fb755231dc88
GianW <gian.cb@hotmail.com>
2023-04-17 00:10:58 -0300

    Usando tema no localstorage

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -6,15 +6,22 @@ import { theme } from './theme'
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} })
 
 function ThemeContext({ children }) {
-  const [mode, setMode] = React.useState('dark')
+  const [mode, setMode] = React.useState(
+    typeof window !== 'undefined' && localStorage?.getItem('theme')
+      ? localStorage.getItem('theme')
+      : 'dark'
+  )
+
+  // console.log(mode)
 
   const colorMode = React.useMemo(
     () => ({
       toggleColorMode: () => {
         setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'))
+        if (typeof window !== 'undefined') localStorage.setItem('theme', mode)
       },
     }),
-    []
+    [mode]
   )
   const userTheme = theme({ mode: mode })
 
