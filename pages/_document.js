9c32c1947e537533c79ea6bfe8e3c83877deafc4
Gian Winckler <gian.cb@hotmail.com>
2024-02-20 16:39:07 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -33,13 +33,17 @@ export default class Document extends NextDocument {
                       `,
                     }}
                   />
-                  <script type="text/javascript">
-                    (function(c,l,a,r,i,t,y){
+                  <script
+                    dangerouslySetInnerHTML={{
+                      __html: `
+                        (function(c,l,a,r,i,t,y){
                         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                    })(window, document, "clarity", "script", "l4v430ub5y");
-                  </script>
+                        })(window, document, "clarity", "script", "l4v430ub5y");
+                      `,
+                    }}
+                  />         
                   <App {...props} />
                 </>
               )
