7c8003c090c50af0c5bb561332edc7451439e90f
Gian Winckler <gian.cb@hotmail.com>
2024-07-05 21:41:45 -0500

    Update packages

--- src/components/post-body.js
+++ src/components/post-body.js
@@ -6,17 +6,11 @@ export const PostBody = ({ content }) => {
   const theme = 'tomorrow'
   return (
     <>
-      <Head>
-        <link
-          rel='preload'
-          href='https://unpkg.com/prismjs@0.0.1/themes/prism-tomorrow.css'
-          as='script'
-        />
+      <Head> 
         <link
           href={`https://unpkg.com/prismjs@0.0.1/themes/prism-${theme}.css`}
           rel='stylesheet'
-        />
-        <link rel='preload' href='./static/post.css' as='script' />
+        />                       
       </Head>
       <div className='max-w-2xl mx-auto' style={{ maxWidth: '900px' }}>
         <div
